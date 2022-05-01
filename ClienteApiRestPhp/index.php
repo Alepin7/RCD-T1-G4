33604e5ef41617f8f79895ae720e8a3a8c32aa12
Alepin7 <alex.pinom@utem.cl>
2022-05-01 13:18:54 -0600

    Cliente Rest terminado por ahora

---
+++ ClienteApiRestPhp/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/index.css">
+    <!--Font awesome-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    
+    <title> Web Service Rest</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info sticky-top p-3 aria-label">
+        <div class="container">
+            <a class="navbar-brand" href="index.php" style="color: white;">Web Service Rest</a>
+            <button class="navbar-toggler border-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <ion-icon name="menu-outline"></ion-icon>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="index.php" style="text-align: center; color: white;">Inicio </a>
+                    </li>
+
+                    <li class="mx-5">
+                        
+                    <a href="src/pages/nombre.php" class="btn btn-outline-light px-2" style="text-align: center; max-width: 850px;">Ingresar un nombre completo</a>
+                    <a href="src/pages/rut.php" class="btn btn-outline-light px-2 ml-1" style="text-align: center; max-width: 850px;">Validar Digito Verificador</a>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section id="inicio">
+        <div class="container">
+            <div class="text pt-5 pb-1">
+                <h1>
+                    <span class="">W</span>
+                    <span class="">e</span>
+                    <span class="">b</span>
+                    <span class="ml-3">S</span>
+                    <span class="">e</span>
+                    <span class="">r</span>
+                    <span class="">v</span>
+                    <span class="">i</span>
+                    <span class="">c</span>
+                    <span class="">e</span>
+                    
+                    <span class="ml-3"></span>
+                    <span class="">R</span>
+                    <span class="">E</span>
+                    <span class="">S</span>
+                    <span class="">T</span>
+                    
+                </h1>
+                <h1 class="divider"></h1>
+
+                <div class="row">
+                    <div class="col-6">
+                        <h5 class="text-justify mt-4 pb-5 mr-4">Hola. Te damos la bienvenida a nuestro Web Service Rest!. A continuaci&oacuten puedes realizar las siguientes operaciones:<br> </h5>
+                        <div class="mb-2">
+                            <a class="mb-5" href="src/pages/nombre.php">Ingresar un nombre completo</a>
+                        </div>
+                        <br>
+                        <div>
+                            <a class="mt-5" id="a2" href="src/pages/rut.php">Validar Digito Verificador</a>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <h5 class="text-justify mt-4 pb-5 mr-4"><br> </h5>
+                        <h5 class="text-justify mt-4 pb-9 mr-4">Trabajo N°1 RCD realizado por el Grupo N°4, integrado por :<br> </h5>
+                        <span class="">• Alex Pino Moya</span>
+                        <h9 class=""><br> </h9>
+                        <span class="">• Martin Martinez Allende</span>
+                        <h9 class=""><br> </h9>
+                        <span class="">• Diego Pizarro Soto</span>
+                        <h9 class=""><br> </h9>
+                        <span class="">• Felipe Jerez Alvarez</span>
+                        <h9 class=""><br> </h9>
+                        <span class="">• Vicente Gallegos Muñoz</span>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c8152ea011.js" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
+
+<?php
+
+
+//Crear cliente
+
+
+//Usar metodos creados
+
